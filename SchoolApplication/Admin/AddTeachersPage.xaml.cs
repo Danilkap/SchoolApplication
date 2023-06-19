f381f5cf8d49a999092c9fbd3c1f4a445dd45dab
Даниил Поздов <pozdov.danil2015@yandex.ru>
2023-06-19 18:14:26 +0300

    Разработка 11

---
+++ SchoolApplication/Admin/AddTeachersPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolApplication.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTeachersPage.xaml
+    /// </summary>
+    public partial class AddTeachersPage : Page
+    {
+        public AddTeachersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
