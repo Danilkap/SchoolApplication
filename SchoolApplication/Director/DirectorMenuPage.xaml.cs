2bef9265d92d5f61b40fde0fd760284b62713eff
Даниил Поздов <pozdov.danil2015@yandex.ru>
2023-12-13 17:58:26 +0300

    Разработка Этап 2

---
+++ SchoolApplication/Director/DirectorMenuPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolApplication.Director
+{
+    /// <summary>
+    /// Логика взаимодействия для DirectorMenuPage.xaml
+    /// </summary>
+    public partial class DirectorMenuPage : Page
+    {
+        public DirectorMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.GoBack();
+        }
+
+        private void BtnViewStudent_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new Director.StudentPage());
+        }
+
+        private void BtnViewTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new Director.TeachersPage());
+        }
+
+        private void BtnViewLesson_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new Director.LessonPage());
+        }
+
+        private void BtnViewSchoolNews_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new Director.SchoolNewsPage());
+        }
+    }
+}
---
+++ SchoolApplication/Director/SchoolNewsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolApplication.Director
+{
+    /// <summary>
+    /// Логика взаимодействия для SchoolNewsPage.xaml
+    /// </summary>
+    public partial class SchoolNewsPage : Page
+    {
+        public SchoolNewsPage()
+        {
+            InitializeComponent();
+
+            DgrNews.ItemsSource = DbConnect.entObj.News.ToList();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.GoBack();
+        }
+    }
+}
